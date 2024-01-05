ad09f2d8917cc274f71ae46337d1ea99faab85c6
Afrina Mustofa Sharony <afrinasharony26@gmail.com>
2024-01-05 21:15:38 +1100

    password hashing

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/requestValidationError";
 import { DatabaseConnectionError } from "../errors/databaseConnectionError";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/badRequestError";
 
 const router = express.Router();
 
@@ -20,10 +22,19 @@ router.post(
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
-    console.log("Creating a user...");
-    throw new DatabaseConnectionError();
 
-    res.send({});
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadRequestError("Email in use");
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
