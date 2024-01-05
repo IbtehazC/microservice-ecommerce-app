b9f8c5a50ea2e4e910ec62d421bea5b6bc4c98e9
Afrina Mustofa Sharony <afrinasharony26@gmail.com>
2024-01-05 15:32:36 +1100

    Custom error handling updated

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,27 @@
 import express from "express";
 import { json } from "body-parser";
+import "express-async-errors";
+
+import { currentUserRouter } from "./routes/currentUser";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middleware/errorHandler";
+import { NotFoundError } from "./errors/notFoundError";
 
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!");
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
 });
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
