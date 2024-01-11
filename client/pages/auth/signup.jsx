e15cfa94117fbec75cace82a0fa428ac7dbaef78
Afrina Mustofa Sharony <afrinasharony26@gmail.com>
2024-01-11 18:17:52 +1100

    signup page and useRequest hook

--- client/pages/auth/signup.jsx
+++ client/pages/auth/signup.jsx
@@ -1,13 +1,23 @@
 import { useState } from "react";
+import Router from "next/router";
+import useRequest from "../../hooks/useRequest";
 
 export default function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signup",
+    method: "post",
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => Router.push("/"),
+  });
 
-  const onSubmit = (e) => {
-    e.preventDefaul();
-
-    console.log(email, password);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await doRequest();
   };
 
   return (
@@ -57,6 +67,7 @@ export default function SignUp() {
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
+            {errors}
             <button
               type="submit"
               className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
