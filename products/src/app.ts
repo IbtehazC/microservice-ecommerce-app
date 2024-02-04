38ae6e8cadb8a247098fcfcccf639f2b9e5de92f
Ibtehaz Ali Chowdhury <ibtehaz.chowdhury@gmail.com>
2024-02-04 12:53:19 +1100

    products service update #2

--- products/src/app.ts
+++ products/src/app.ts
@@ -4,6 +4,9 @@ import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@ibtehazc/common";
 import { createProductRouter } from "./routes/new";
+import { showProductRouter } from "./routes/show";
+import { indexProductRouter } from "./routes/index";
+import { updateProductRouter } from "./routes/update";
 
 const app = express();
 app.set("trust proxy", true);
@@ -14,9 +17,12 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
-
 app.use(currentUser);
+
 app.use(createProductRouter);
+app.use(showProductRouter);
+app.use(indexProductRouter);
+app.use(updateProductRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
